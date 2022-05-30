60172790d678764d9865e53eeaa14a85f83aecaa
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2022-05-29 23:19:33 -0300

    feat: automatizando construcao das pastas

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
+from os.path import exists, getsize
+from organizador import contruir_estrutura
 
+if (exists("dt_treino") == False) or getsize("dt_treino") == 0 or getsize("dt_treino/affenpinscher") == 0:
+    contruir_estrutura("dt_treino", "dataset/train/")
+
+# if (exists("dt_validacao") == False) or getsize("dt_validacao") == 0 or getsize("dt_validacao/affenpinscher") == 0:
+#     contruir_estrutura("dt_validacao", "dataset/test/")    
 
 image_size = (224, 224)
 # quantidade de exempplos de treino que serao usados em 1 iteraçao
@@ -9,7 +16,7 @@ image_size = (224, 224)
 batch_size = 32
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "new_dataset",
+    "dt_treino",
     validation_split=0.2,
     subset="training",
     color_mode="grayscale",
@@ -18,7 +25,7 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size,
 )
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "new_dataset",
+    "dt_treino",
     validation_split=0.2,
     subset="validation",
     color_mode="grayscale",
@@ -28,3 +35,5 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 )
 
 print()
+
+
--- organizador.py
+++ organizador.py
@@ -4,28 +4,31 @@ import os
 import shutil
 
 
-etiquetas_csv = pd.read_csv("dataset/labels.csv")
-caminho_dt_treino = "dataset/train/"
+def contruir_estrutura(nova_pasta_origem, origem):
 
-nome_arquivos = [caminho_dt_treino + nome_arquivo + ".jpg" for nome_arquivo in etiquetas_csv["id"]]
-etiquetas = etiquetas_csv["breed"].to_numpy() # convert labels column to NumPy array
+    etiquetas_csv = pd.read_csv("dataset/labels.csv")
+    caminho_dt_treino = origem
 
-# cria a pasta para o novo dataset organizado por raças
-os.mkdir("new_dataset")
+    nome_arquivos = [caminho_dt_treino + nome_arquivo + ".jpg" for nome_arquivo in etiquetas_csv["id"]]
+    etiquetas = etiquetas_csv["breed"].to_numpy() # convert labels column to NumPy array
 
-# cria as pastas de cada raca
-for raca_cachorro in np.unique(etiquetas):
-    nova_pasta = raca_cachorro
-    endereco_pasta = "new_dataset"
-    endereco_novo = os.path.join(endereco_pasta, nova_pasta)
-    os.mkdir(endereco_novo)
+    # cria a pasta para o novo dataset organizado por raças
+    if os.path.exists(nova_pasta_origem) == False:
+        os.mkdir(nova_pasta_origem)
+
+    # cria as pastas de cada raca
+    for raca_cachorro in np.unique(etiquetas):
+        nova_pasta = raca_cachorro
+        endereco_novo = os.path.join(nova_pasta_origem, nova_pasta)
+        if os.path.exists(endereco_novo) == False:
+            os.mkdir(endereco_novo)
 
 
-# move as imagens para sua determinada pasta
-for index in range(len(nome_arquivos)):
-    endereco_imagem = nome_arquivos[index]
-    nome_raca = etiquetas[index]
-    src_path = endereco_imagem
-    dst_path = os.path.join("new_dataset", nome_raca)
-    shutil.move(src_path, dst_path)
+    # move as imagens para sua determinada pasta
+    for index in range(len(nome_arquivos)):
+        endereco_imagem = nome_arquivos[index]
+        nome_raca = etiquetas[index]
+        src_path = endereco_imagem
+        dst_path = os.path.join(nova_pasta_origem, nome_raca)
+        shutil.move(src_path, dst_path)
 
