69d151ade292342f4ed1e09dc52f0803586b9b4c
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2022-06-05 01:58:54 -0300

    feat: new model and organization files
    Co-authored-by: Lucas da Silva dos Santos <Lds20k@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
 import os
+import random
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 from genericpath import exists, getsize
 from keras import layers
-from tensorflow import keras
+from keras.layers import (Conv2D, Dense, Dropout, Flatten, Input, MaxPooling2D,
+                          Rescaling, ZeroPadding2D)
+from keras.losses import categorical_crossentropy
+from keras.models import Sequential
+from keras.preprocessing.image import (ImageDataGenerator, img_to_array,
+                                       load_img)
+from organizador import contruir_estrutura, ler_racas
 
-from organizador import contruir_estrutura
+tf.config.threading.set_intra_op_parallelism_threads(4)
+tf.config.threading.set_inter_op_parallelism_threads(4)
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# hiperparametros
+image_width = 100
+image_height = 100
+image_color_channel = 3
+image_color_channel_size = 255
+image_size = (image_width, image_height)
+image_shape = image_size + (image_color_channel,)
+
+batch_size = 32
+# numero de passos para o treinamento da ML terminar
+# https://radiopaedia.org/articles/epoch-machine-learning#:~:text=An%20epoch%20is%20a%20term,of%20data%20is%20very%20large).
+epochs = 1
+learning_rate = 1e-2
 
 if (exists("dt_treino") == False) or getsize("dt_treino") == 0 or getsize("dt_treino/affenpinscher") == 0:
-    contruir_estrutura("dt_treino", "train/")
+    contruir_estrutura()
 
-# if (exists("dt_validacao") == False) or getsize("dt_validacao") == 0 or getsize("dt_validacao/affenpinscher") == 0:
-#     contruir_estrutura("dt_validacao", "test/")    
-
-image_size = (224, 224)
-# quantidade de exempplos de treino que serao usados em 1 iteraçao
-# tipo "mini-batch mode" -> maior que um porem menor que o range do dataset usado
-# geralmente é um numero que pode ser dividido pelo tamanho total do dataset
-# https://radiopaedia.org/articles/batch-size-machine-learning?lang=gb
-batch_size = 32
+racas = ler_racas()
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     "dt_treino",
-    validation_split=0.2,
-    subset="training",
-    #color_mode="grayscale",
-    seed=123,
+    # validation_split=0.2,
+    # subset="training",
     image_size=image_size,
     batch_size=batch_size,
+    label_mode='categorical',
+    labels='inferred',
+    class_names=racas
 )
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "dt_treino",
-    validation_split=0.2,
-    subset="validation",
-    #color_mode="grayscale",
-    seed=123,
+    "dt_validacao",
     image_size=image_size,
     batch_size=batch_size,
+    label_mode='categorical',
+    labels='inferred',
+    class_names=racas
 )
 
-data_augmentation = keras.Sequential(
+# aumenta a variabilidade do dataset fazendo algumas alterações aleatorias
+aumentar_dataset = keras.Sequential(
     [
         layers.RandomFlip("horizontal"),
-        layers.RandomRotation(0.1),
+        layers.RandomRotation(0.5),
+        layers.RandomZoom(0.5),
     ]
 )
 
@@ -56,7 +72,7 @@ data_augmentation = keras.Sequential(
 # plt.figure(figsize=(10, 10))
 # for images, _ in train_ds.take(1):
 #     for i in range(9):
-#         augmented_images = data_augmentation(images)
+#         augmented_images = aumentar_dataset(images)
 #         ax = plt.subplot(3, 3, i + 1)
 #         plt.imshow(augmented_images[0].numpy().astype("uint8"))
 #         plt.axis("off")
@@ -66,111 +82,189 @@ data_augmentation = keras.Sequential(
 
 # normaliza a imagem de 255,255,255 para escala de 0 à 1
 augmented_train_ds = train_ds.map(
-  lambda x, y: (data_augmentation(x, training=True), y))
-
+   lambda x, y: (aumentar_dataset(x, training=True), y))
 
 train_ds = train_ds.prefetch(buffer_size=32)
 val_ds = val_ds.prefetch(buffer_size=32)
 
 
-def make_model(input_shape, num_classes):
-    
-    inputs = keras.Input(shape=input_shape)
-    # Image augmentation block
-    x = data_augmentation(inputs)
+def make_model(input_shape):
+    with tf.device('/CPU:0'):
+        inputs = keras.Input(shape=input_shape)
+        x = aumentar_dataset(inputs)
 
-    # Entry block
-    x = layers.Rescaling(1.0 / 255)(x)
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+        x = Conv2D(32, 5, activation='relu')(x)
+        x = MaxPooling2D()(x)
+        
+        x = Conv2D(64, 4, activation='relu')(x)
+        x = MaxPooling2D()(x)
 
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+        x = Conv2D(128, 3, activation='relu')(x)
+        x = MaxPooling2D()(x)
+        x = Dropout(0.5)(x)
 
-    previous_block_activation = x  # Set aside residual
+        x = Flatten()(x)
+        x = Dense(500, activation='relu')(x)
+        x = Dropout(0.5)(x)
 
-    for size in [128, 256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        activation = "sigmoid"
-        units = 1
-    else:
-        activation = "softmax"
-        units = num_classes
-
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
+        outputs = layers.Dense(120, activation="softmax")(x)
+        return keras.Model(inputs, outputs)
 
 
-model = make_model(input_shape=image_size + (3,), num_classes=2)
-keras.utils.plot_model(model, show_shapes=True)
+model = make_model(image_shape)
 
-# numero de passos para o treinamento da ML terminar
-# https://radiopaedia.org/articles/epoch-machine-learning#:~:text=An%20epoch%20is%20a%20term,of%20data%20is%20very%20large).
-epochs = 1
+
+
+# with tf.device('/CPU:0'):
+#     model = tf.keras.models.Sequential([
+#             tf.keras.layers.experimental.preprocessing.Rescaling(
+#                 1./ 255.0,
+#                 input_shape = image_size + (3,)
+#             ),
+#             aumentar_dataset,
+#             tf.keras.layers.Conv2D(16, 3, padding = 'same', activation='relu'),
+#             tf.keras.layers.MaxPooling2D(),
+            
+#             tf.keras.layers.Conv2D(32, 3, padding = 'same', activation='relu'),
+#             tf.keras.layers.MaxPooling2D(),
+            
+#             tf.keras.layers.Conv2D(64, 3, padding = 'same', activation='relu'),
+#             tf.keras.layers.MaxPooling2D(),
+            
+#             tf.keras.layers.Flatten(),            
+
+#             # tf.keras.layers.Dense(20, activation = 'relu'),
+
+#             tf.keras.layers.Dropout(0.1),
+#             tf.keras.layers.Dense(1, activation = 'sigmoid')
+#         ])
+
+model.summary()
 
 callbacks = [
+    keras.callbacks.EarlyStopping(monitor = 'loss', patience = 3),
     keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
 ]
 
 model.compile(
-    optimizer=keras.optimizers.Adam(1e-3),
-    loss="binary_crossentropy",
+    optimizer=keras.optimizers.Adam(learning_rate),
+    loss=tf.keras.losses.CategoricalCrossentropy(),
     metrics=["accuracy"],
 )
 
-print(model.summary())
+# Precisa instalar o Graphviz, para rodar a linha abaixo
+# só necessário rodar quando alterar o modelo
+# keras.utils.plot_model(model, show_shapes=True)
 
-model.fit(
-    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
-)
+historico = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
 
-img = keras.preprocessing.image.load_img(
-    "PetImages/Cat/6779.jpg", target_size=image_size
-)
+#Plota os graficos sobre o fit
+# plt.figure(0)
+# plt.plot(historico.history['accuracy'], label='training accuracy')
+# plt.plot(historico.history['val_accuracy'], label='val accuracy')
+# plt.title('Accuracy')
+# plt.xlabel('epochs')
+# plt.ylabel('accuracy')
+# plt.legend()
+# plt.show()
+# plt.figure(1)
+# plt.plot(historico.history['loss'], label='training loss')
+# plt.plot(historico.history['val_loss'], label='val loss')
+# plt.title('Loss')
+# plt.xlabel('epochs')
+# plt.ylabel('loss')
+# plt.legend()
+# plt.show()
 
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+# Segunda maneira
+accuracy = historico.history['accuracy']
+val_accuracy = historico.history['val_accuracy']
+loss = historico.history['loss']
+val_loss = historico.history['val_loss']
+epochs_range = range(epochs)
 
-predictions = model.predict(img_array)
-score = predictions[0]
+plt.gcf().clear()
+plt.figure(figsize = (15, 8))
 
-res_str = "This image is %.2f percent cat and %.2f percent dog." % (100 * (1 - score), 100 * score)
+plt.subplot(1, 2, 1)
+plt.title('Training and Validation Accuracy')
+plt.plot(epochs_range, accuracy, label = 'Training Accuracy')
+plt.plot(epochs_range, val_accuracy, label = 'Validation Accuracy')
+plt.legend(loc = 'lower right')
 
-print(res_str)
-
-plt.figure(figsize=(5, 5))
-# ax = plt.subplot(3)
-plt.title("Imagem 6779")
-plt.imshow(img)
-# plt.text(0.5, 0, res_str , fontsize=20,
-#      horizontalalignment='center')
-plt.axis("off")
+plt.subplot(1, 2, 2)
+plt.title('Training and Validation Loss')
+plt.plot(epochs_range, loss, label = 'Training Loss')
+plt.plot(epochs_range, val_loss, label = 'Validation Loss')
+plt.legend(loc = 'lower right')
 
 plt.show()
 
 
+dataset_test_loss, dataset_test_accuracy = model.evaluate(val_ds)
+
+print('Dataset Test Loss:     %s' % dataset_test_loss)
+print('Dataset Test Accuracy: %s' % dataset_test_accuracy)
+
+
+def plot_dataset_predictions(dataset):
+
+    features, labels = val_ds.as_numpy_iterator().next()
+
+    predictions = model.predict_on_batch(features).flatten()
+    # predictions = tf.where(predictions < 0.5, 0, 1)
+
+    print('Labels:      %s' % labels[0])
+    # print('Index:      %s' % labels[0].index(1))
+    print('Predictions: %s' % predictions.numpy())
+
+    plt.gcf().clear()
+    plt.figure(figsize = (15, 15))
+
+    for i in range(9):
+
+        plt.subplot(3, 3, i + 1)
+        plt.axis('off')
+
+        plt.imshow(features[i].astype('uint8'))
+        plt.title(racas[predictions[i]])
+
+plot_dataset_predictions(train_ds)
+
+model.save("model_folder")
+
+modelo_salvo = tf.keras.models.load_model('model_folder')
+
+def predict(image_file):
+
+    image = tf.keras.preprocessing.image.load_img(image_file, target_size = image_size)
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.expand_dims(image, 0)
+
+    # prediction = model.predict(image)[0][0]
+    prediction = model.predict(image)
+    print()
+
+
+    # print('Prediction: {0} | {1}'.format(prediction, ('cat' if prediction < 0.5 else 'dog')))
+
+def predict_url(image_fname, image_origin):
+    image_file = tf.keras.utils.get_file(image_fname, origin = image_origin)
+    return predict(image_file)
+
+def predict_ramdom():
+    arquivos_test = os.listdir("dataset/test/")
+    index_imagem = random.randrange(0, len(arquivos_test))
+
+    nome_imagem = arquivos_test[index_imagem]
+    image = tf.keras.preprocessing.image.load_img("dataset/test/" + nome_imagem, target_size = image_size)
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.expand_dims(image, 0)
+
+    # prediction = model.predict(image)[0][0]
+    prediction = model.predict(image)
+    print()
+
+predict('dataset/test/0a4ef19459cd2100977b052de5f46231.jpg')
+predict_url('Imagem_selecionada', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Black-Magic-Big-Boy.jpg')
+predict_ramdom()
--- organizador.py
+++ organizador.py
@@ -1,39 +1,74 @@
-import numpy as np
-import pandas as pd
+import math
 import os
 import shutil
 
+import numpy as np
+import pandas as pd
 
-def contruir_estrutura(nova_pasta_origem, origem):
-
-    if os.path.exists("dataset/dog-breed-identification") == True:
-        origem = os.path.join("dataset/dog-breed-identification",origem)
-        etiquetas_csv = pd.read_csv("dataset/dog-breed-identification/labels.csv")
+def carregar_dataset():
+    if os.path.exists("dataset/dog-breed-identification"):
+        origem = "dataset/dog-breed-identification/"
+        etiquetas_csv = pd.read_csv(origem + "labels.csv")
     else:
-        origem = os.path.join("dataset",origem)
-        etiquetas_csv = pd.read_csv("dataset/labels.csv")
-    caminho_dt_treino = origem
+        origem = "dataset/"
+        etiquetas_csv = pd.read_csv( origem + "labels.csv")
+    
+    return origem, etiquetas_csv
 
-    nome_arquivos = [caminho_dt_treino + nome_arquivo + ".jpg" for nome_arquivo in etiquetas_csv["id"]]
-    etiquetas = etiquetas_csv["breed"].to_numpy() # convert labels column to NumPy array
+def ler_racas():
+    origem, etiquetas_csv = carregar_dataset()
+    etiquetas = etiquetas_csv["breed"].to_numpy()
+    # return np.unique(etiquetas).ravel()
+    return list(np.unique(etiquetas))
 
-    # cria a pasta para o novo dataset organizado por raças
-    if os.path.exists(nova_pasta_origem) == False:
-        os.mkdir(nova_pasta_origem)
+
+def contruir_estrutura():
+    origem, etiquetas_csv = carregar_dataset()
+    
+    etiquetas = etiquetas_csv["breed"].to_numpy()
+
+    racas = np.unique(etiquetas)
+    cachorro_raca_geral = {}
+
+    for raca in racas:
+        cachorro_raca_geral[raca] = etiquetas_csv.loc[etiquetas_csv['breed'] == raca]['id'].to_numpy()
+    
+    cachorro_raca_treino = {}
+    cachorro_raca_validacao = {}
+
+    for raca in racas:
+        quantidade_total = len(cachorro_raca_geral[raca])
+        quantidade_treino = math.floor(quantidade_total * 0.8)
+
+        cachorro_raca_treino[raca] = cachorro_raca_geral[raca][0:quantidade_treino]
+        cachorro_raca_validacao[raca] = cachorro_raca_geral[raca][quantidade_treino:]
+
+    # cria a pasta dt_treino para o novo dataset organizado por raças
+    if not os.path.exists("dt_treino"):
+        os.mkdir("dt_treino")
+
+    # cria a pasta dt_validacao para o novo dataset organizado por raças
+    if not os.path.exists("dt_validacao"):
+        os.mkdir("dt_validacao")
 
     # cria as pastas de cada raca
-    for raca_cachorro in np.unique(etiquetas):
-        nova_pasta = raca_cachorro
-        endereco_novo = os.path.join(nova_pasta_origem, nova_pasta)
-        if os.path.exists(endereco_novo) == False:
-            os.mkdir(endereco_novo)
+    for raca in racas:
+        pasta_raca_treino = os.path.join("dt_treino", raca)
+        pasta_raca_validacao = os.path.join("dt_validacao", raca)
 
+        if not os.path.exists(pasta_raca_treino):
+            os.mkdir(pasta_raca_treino)
+        
+        if not os.path.exists(pasta_raca_validacao):
+            os.mkdir(pasta_raca_validacao)
 
-    # move as imagens para sua determinada pasta
-    for index in range(len(nome_arquivos)):
-        endereco_imagem = nome_arquivos[index]
-        nome_raca = etiquetas[index]
-        src_path = endereco_imagem
-        dst_path = os.path.join(nova_pasta_origem, nome_raca)
-        shutil.move(src_path, dst_path)
-
+    for raca in racas:
+        for id_imagem in cachorro_raca_treino[raca]:
+            endereco_imagem = f"{origem}train/{id_imagem}.jpg"
+            pasta_raca = f"dt_treino/{raca}"
+            shutil.move(endereco_imagem, pasta_raca)
+        
+        for id_imagem in cachorro_raca_validacao[raca]:
+            endereco_imagem = f"{origem}train/{id_imagem}.jpg"
+            pasta_raca = f"dt_validacao/{raca}"
+            shutil.move(endereco_imagem, pasta_raca)
